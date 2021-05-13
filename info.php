169b9e011255ed9c017702af44471d0de2134dc0
Varun <54104000+varunrana8@users.noreply.github.com>
2021-05-14 02:43:57 +0530

    Update info.php

--- info.php
+++ info.php
@@ -1,6 +1,14 @@
 <?php
   
-  require 'PHPMailerAutoload.php';
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+
+  require 'vendor/phpmailer/phpmailer/src/Exception.php';
+  require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+  require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+  // Include autoload.php file
+  require 'vendor/autoload.php';
   
   // Create object of PHPMailer class
   $mail = new PHPMailer(true);
